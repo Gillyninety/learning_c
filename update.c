859c2844f0a52ca099ee055c1eba01cec0b57543
Gillyninety <121914333+Gillyninety@users.noreply.github.com>
2023-12-06 12:06:37 +0000

    Add files via upload
    First attempt at Linux update program in C.

---
+++ update.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	setuid(geteuid());
+	system("sudo apt-get update -y && sudo apt update -y && sudo apt upgrade -y");
+}
+
