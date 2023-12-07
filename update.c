d0992c1fe55571f0ed2db3e93ab6233abc6eb5f9
Gillyninety <121914333+Gillyninety@users.noreply.github.com>
2023-12-07 11:02:30 +0000

    Update update.c
    Added ability to choose between standard and full OS update.

--- update.c
+++ update.c
@@ -3,8 +3,37 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(){
+void normalUpdate(){
 	//setuid(geteuid());
 	system("sudo apt-get update -y && sudo apt update -y && sudo apt upgrade -y");
 }
 
+void fullUpgrade(){
+	//setuid(geteuid());
+	system("sudo apt-get update -y && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade");
+}
+
+
+int main(void){
+	int selection;
+	while(selection != 1 &&  selection != 2){
+	puts("Enter 1 to update\nEnter 2 for full OS upgrade");
+	scanf("%d", &selection);
+	}
+	
+	switch(selection)
+	{
+	case 1:
+		puts("Standard update chosen\n");
+		normalUpdate();
+		break;
+	case 2:
+		puts("Full OS upgrade chosen\n");
+		fullUpgrade();
+		break;
+	default:
+		puts("Undefined error, please try again");
+	
+	}
+	return 0;
+}
